aa641812144761bdcc220aa1a0f5004fdaffc0d2
grubbywww <wangyj@easemob.com>
2017-01-21 02:53:11 +0800

    html view

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,5 @@
 function create(){
     phone=$("#phone").val();
     content=$("#content").val();
-    $("#sample_6").find("tr:eq(0)").after("<tr><td>"+phone+"</td><td>"+content+"</td><td></td></tr>")
+    $("#sample_6 tbody").append("<tr><td>"+phone+"</td><td>"+content+"</td><td></td></tr>")
 }
