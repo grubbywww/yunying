007d6d5efda47907750378081bb8dc2ebe6f1a6f
grubbywww <wangyj@easemob.com>
2017-01-21 03:37:16 +0800

    html view

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,11 @@
 function create(){
+
     phone=$("#phone").val();
     content=$("#content").val();
-    $("#sample_6 tbody").append("<tr><td>"+phone+"</td><td>"+content+"</td><td></td></tr>")
+    if (phone==""){
+        alert("联系方式不能为空");
+        return false;
+    }
+    $("#sample_6 tbody").append("<tr><td>"+phone+"</td><td>"+content+"</td><td></td></tr>");
+
 }
